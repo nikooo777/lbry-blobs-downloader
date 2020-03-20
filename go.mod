d343d02d70ea9aaa71a4fed2d9ad33d3e09e2d19
Niko Storni <niko@lbry.io>
2020-03-20 02:41:32 +0100

    add support for downloading streams

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module blobdownloader
 go 1.13
 
 require (
-	github.com/lbryio/lbry.go/v2 v2.4.0
-	github.com/lbryio/reflector.go v1.1.3-0.20191216173656-d89b58c52a14
+	github.com/lbryio/lbry.go/v2 v2.4.5
+	github.com/lbryio/reflector.go v1.1.3-0.20200319161139-1c4ea4d5a4aa
 	github.com/sirupsen/logrus v1.4.2
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"io/ioutil"
 	"os"
 	"strconv"
@@ -8,6 +9,7 @@ import (
 	"time"
 
 	"github.com/lbryio/lbry.go/v2/extras/errors"
+	"github.com/lbryio/lbry.go/v2/stream"
 	"github.com/lbryio/reflector.go/peer"
 	"github.com/sirupsen/logrus"
 )
@@ -16,6 +18,8 @@ var reflectorServer = "refractor.lbry.com:5567"
 
 func main() {
 	concurrentThreads := int64(20)
+	isStream := false
+	var err error
 	if len(os.Args) < 2 {
 		logrus.Errorln("you must specify a blob hash to download")
 		os.Exit(1)
@@ -24,44 +28,69 @@ func main() {
 		reflectorServer = os.Args[2]
 	}
 	if len(os.Args) >= 4 {
-		var err error
-		concurrentThreads, err = strconv.ParseInt(os.Args[3], 10, 32)
+		isStream, err = strconv.ParseBool(os.Args[3])
 		if err != nil {
 			logrus.Errorln(err.Error())
 			os.Exit(1)
 		}
 	}
+	if len(os.Args) >= 5 {
+		var err error
+		concurrentThreads, err = strconv.ParseInt(os.Args[4], 10, 32)
+		if err != nil {
+			logrus.Errorln(err.Error())
+			os.Exit(1)
+		}
+	}
+	logrus.Println(concurrentThreads)
 	wg := &sync.WaitGroup{}
 	for i := int64(0); i < concurrentThreads; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			err := DownloadBlob(os.Args[1])
-			if err != nil {
-				logrus.Error(errors.FullTrace(err))
-				os.Exit(1)
+			if isStream {
+				blob, err := DownloadBlob(os.Args[1])
+				if err != nil {
+					logrus.Error(errors.FullTrace(err))
+					os.Exit(1)
+				}
+				sdb := &stream.SDBlob{}
+				err = sdb.FromBlob(*blob)
+
+				if err != nil {
+					logrus.Error(errors.FullTrace(err))
+					os.Exit(1)
+				}
+				DownloadStream(sdb)
+			} else {
+				_, err := DownloadBlob(os.Args[1])
+				if err != nil {
+					logrus.Error(errors.FullTrace(err))
+					os.Exit(1)
+				}
 			}
 		}()
 	}
 	wg.Wait()
 }
 
-func DownloadBlob(hash string) error {
+func DownloadBlob(hash string) (*stream.Blob, error) {
 	bStore := GetBlobStore()
 	start := time.Now()
 	blob, err := bStore.Get(hash)
 	if err != nil {
-		return errors.Err(err)
+		err = errors.Prefix(hash, err)
+		return nil, errors.Err(err)
 	}
 	elapsed := time.Since(start)
 	logrus.Infof("download time: %d ms\tSpeed: %.2f MB/s", elapsed.Milliseconds(), (float64(len(blob))/(1024*1024))/elapsed.Seconds())
 	err = ioutil.WriteFile(hash, blob, 0644)
 	if err != nil {
-		return errors.Err(err)
+		return nil, errors.Err(err)
 	}
 	elapsed = time.Since(start) - elapsed
 	//logrus.Infof("save time: %d us\tSpeed: %.2f MB/s", elapsed.Microseconds(), (float64(len(blob))/(1024*1024))/elapsed.Seconds())
-	return nil
+	return &blob, nil
 }
 
 // GetBlobStore returns default pre-configured blob store.
@@ -70,4 +99,27 @@ func GetBlobStore() *peer.Store {
 		Address: reflectorServer,
 		Timeout: 30 * time.Second,
 	})
+}
+
+func DownloadStream(blob *stream.SDBlob) {
+	hashes := GetStreamHashes(blob)
+	for _, hash := range hashes {
+		logrus.Info(hash)
+		_, err := DownloadBlob(hash)
+		if err != nil {
+			logrus.Error(errors.FullTrace(err))
+		}
+	}
+}
+
+func GetStreamHashes(blob *stream.SDBlob) []string {
+	blobs := make([]string, 0, len(blob.BlobInfos))
+	for _, b := range blob.BlobInfos {
+		hash := hex.EncodeToString(b.BlobHash)
+		if hash == "" {
+			continue
+		}
+		blobs = append(blobs, hex.EncodeToString(b.BlobHash))
+	}
+	return blobs
 }
