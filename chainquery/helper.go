6b62afad966c73116ba4875182251a7e3d802891
Niko Storni <niko@odysee.com>
2024-07-16 18:55:35 +0200

    add cq helpers

--- chainquery/helper.go
+++ chainquery/helper.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 
 	"github.com/lbryio/lbry.go/v2/extras/errors"
 )
@@ -58,4 +59,107 @@ func GetOriginalName(sdHash string) (string, error) {
 	}
 
 	return cQResp.Data[0].SourceName, nil
+}
+
+func GetSdHash(claimId string) (string, error) {
+	url := fmt.Sprintf("https://chainquery.odysee.tv/api/sql?query=select%%20sd_hash%%20from%%20claim%%20where%%20claim_id%%20=%%20%%27%s%%27", claimId)
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return "", errors.Err(err)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return "", errors.Err(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return "", errors.Err("Failed to get sd hash")
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", errors.Err(err)
+	}
+
+	type cQResponse struct {
+		Success bool        `json:"success"`
+		Error   interface{} `json:"error"`
+		Data    []struct {
+			SdHash string `json:"sd_hash"`
+		} `json:"data"`
+	}
+
+	var cQResp cQResponse
+	err = json.Unmarshal(body, &cQResp)
+	if err != nil {
+		return "", errors.Err(err)
+	}
+
+	if !cQResp.Success {
+		return "", errors.Err("Failed to get sd hash")
+	}
+
+	if len(cQResp.Data) == 0 {
+		return "", errors.Err("No data returned")
+	}
+
+	return cQResp.Data[0].SdHash, nil
+}
+
+// channelClaimId returns all unspent claim_ids and sd_hashes of streams belonging to a channel
+type stream struct {
+	ClaimId string `json:"claim_id"`
+	SdHash  string `json:"sd_hash"`
+}
+
+func GetChannelStreams(channelClaimId string) ([]stream, error) {
+	query := url.QueryEscape(fmt.Sprintf("select claim_id,sd_hash from claim where publisher_id='%s' and bid_state != 'spent' and type = 'stream'", channelClaimId))
+	url := fmt.Sprintf("https://chainquery.odysee.tv/api/sql?query=%s", query)
+	method := "GET"
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return nil, errors.Err(err)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, errors.Err(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, errors.Err("Failed to get streams")
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, errors.Err(err)
+	}
+
+	type cQResponse struct {
+		Success bool        `json:"success"`
+		Error   interface{} `json:"error"`
+		Data    []stream    `json:"data"`
+	}
+	var cQResp cQResponse
+	err = json.Unmarshal(body, &cQResp)
+	if err != nil {
+		return nil, errors.Err(err)
+	}
+
+	if !cQResp.Success {
+		return nil, errors.Err("Failed to get sd hash")
+	}
+
+	if len(cQResp.Data) == 0 {
+		return nil, errors.Err("No data returned")
+	}
+
+	return cQResp.Data, nil
 }
--- chainquery/helper_test.go
+++ chainquery/helper_test.go
@@ -11,3 +11,15 @@ func TestGetOriginalName(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "i-tried-finding-hidden-gems-on.mp4", name)
 }
+
+func TestGetSdHash(t *testing.T) {
+	sdHash, err := GetSdHash("05dbe782f1d8588251b80365610eda80920d8278")
+	assert.Nil(t, err)
+	assert.Equal(t, "323be2060c9f6c7877afc5feec4eb4c0a35eec00ec6439a47a910e6379f58090d77d0a26e6023fea2ea792244ded4e49", sdHash)
+}
+
+func TestGetChannelStreams(t *testing.T) {
+	streams, err := GetChannelStreams("80d2590ad04e36fb1d077a9b9e3a8bba76defdf8")
+	assert.Nil(t, err)
+	assert.Greater(t, len(streams), 10)
+}
